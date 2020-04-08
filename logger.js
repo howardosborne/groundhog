ec212859c20de42b7f4bcd48b6982a7dac9f870d
Howard Osborne <hosborne@etesting.com>
2020-04-08 15:59:59 +0100

    tweaked readme

--- logger.js
+++
@@ -1,8 +0,0 @@
-
-var logFilename = Date.now().toString() + ".log";
-function logResult(result){
-    var timestamp = Date.now().toString();
-    var logEntry = timestamp + "," + result.outcome + "," + result.statusCode + "," + result.hostname + "," + result.port + "," + result.path + "," + result.method
-    console.log("logging result to ${logFilename}")
-    require('fs').appendFileSync(logFilename, logEntry + "\n");
-}
